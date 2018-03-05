5a99c4f63f2bb8ae91ecba74380d18b87d7e3caf
Roman Lopez <roman.lopez93@yahoo.com>
2018-03-05 09:41:19 -0600

    changed/moved a couple files.

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
 const express = require('express');
 const socket = require('socket.io');
-const ejs = require('ejs') // not sure if i wanna keep using ejs
+const ejs = require('ejs'); // not sure if i wanna keep using ejs
 const routes = require('./routes/routing');
 const landing_pages = require('./routes/landing');
-const google_oauth = require('./routes/google_oauth')
+const google_oauth = require('./routes/google_oauth');
 
+// setup the webapp
 var app = express();
 app.use(express.static('public')); // sets pwd
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
 
 // routing
-app.use('/home', routes)
-app.use('/', landing_pages)
-app.use('google/', google_oauth)
+app.use('/home', routes);
+app.use('/', landing_pages); // probably won't need this in the future
+app.use('google/', google_oauth);
 
 // starts the node server
 var server = app.listen(3000, function(){
@@ -23,7 +24,7 @@ var server = app.listen(3000, function(){
 var io = socket(server);
 io.on('connection', (socket) =>{
   // only comes out when we have contacted the client too
-	console.log('made socket connection', socket.id)
+	console.log('made socket connection', socket.id);
 
 	socket.on('disconnect', ()=>{
 		// handle case when a user leaves the app
