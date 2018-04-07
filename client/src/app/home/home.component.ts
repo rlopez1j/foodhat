f08104394530eddc1ccb08f5c062bcee2dd2854f
Roman Lopez <roman.lopez93@yahoo.com>
2018-04-06 22:29:06 -0500

    minor changes

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -6,13 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-<<<<<<< HEAD
   user = 'temp'; // this will come from a db object
 
-=======
-  user = 'temp';
-  
->>>>>>> cfe611fae555db3150f40e73f884460819d1fd4a
   constructor() { }
 
   ngOnInit() {
