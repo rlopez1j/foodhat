f43ddaa5ace504e4a4c186b9936abd125c13b525
Roman Lopez <roman.lopez93@yahoo.com>
2018-06-06 13:25:12 -0500

    updated to Angular 6. Added Material libary.

--- client/src/app/api.service.ts
+++ client/src/app/api.service.ts
@@ -1,14 +1,14 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject'
-import 'rxjs/add/operator/map';
+//import 'rxjs/add/operator/map';
 
 
 
 @Injectable()
 export class ApiService{
 
-  constructor(private http: Http){}
+  constructor(private http: HttpClient){}
   // creates an observable to avoid having to call to api multiple times for user-nfo
   private user = null
 
@@ -22,26 +22,26 @@ export class ApiService{
   // create user for application or log them in. Backend API handles which to do
   getProfile(){
     return this.http.get('api/crud/profile')
-    .map(res => res.json())
+    //.map(res => res.json())
   }
 
   checkUsername(username){
     return this.http.get('api/crud/check-username', {params: {username: username}})
-    .map(res => res.json())
+    //.map(res => res.json())
   }
 
   createUsername(username){
     return this.http.post('api/crud/create-username', {username: username})
-    .map(res => res.json())
+    //.map(res => res.json())
   }
 
   getHistory(){
     return this.http.get('api/crud/get-history')
-    .map(res => res.json())
+    //.map(res => res.json())
   }
 
   getFriendsList(){
     return this.http.get('api/crud/get-friends-list')
-    .map(res => res.json())
+    //.map(res => res.json())
   }
 }
